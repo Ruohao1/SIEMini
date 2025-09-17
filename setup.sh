c682c0e53d0c802def1e04b0d85d91eedbe9e334
ruohao <ruohaolin@mgmail.com>
2025-09-17 17:06:21 -0400

    debug

--- setup.sh
+++ setup.sh
@@ -5,27 +5,34 @@ eval set -- "$OPTS"
 
 while true; do
   case "$1" in
-    -h|--host) FILE="$2"; shift 2 ;;
-    --) shift; break ;;
-    *) break ;;
+  -h | --host)
+    FILE="$2"
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *) break ;;
   esac
 done
 
 case "$HOST" in
-  web)
-    echo "Running web server setup..."
-    ./setup_web.sh
-    ;;
-  siem)
-    echo "Running SIEM setup..."
-    ./setup_siem.sh
-    ;;
-  attacker)
-    echo "Running attacker VM setup..."
-    ./setup_attacker.sh
-    ;;
-  *)
-    echo "Unknown host: $HOST"
-    echo "Valid options are: web, siem, attacker"
-    exit 1
-    ;;
+web)
+  echo "Running web server setup..."
+  ./setup_web.sh
+  ;;
+siem)
+  echo "Running SIEM setup..."
+  ./setup_siem.sh
+  ;;
+attacker)
+  echo "Running attacker VM setup..."
+  ./setup_attacker.sh
+  ;;
+*)
+  echo "Unknown host: $HOST"
+  echo "Valid options are: web, siem, attacker"
+  exit 1
+  ;;
+esac
